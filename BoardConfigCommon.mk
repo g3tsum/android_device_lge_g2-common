063a0ceae17a2a1137d9168af77478680852557b
houst0nn <houstonn@gmail.com>
2013-11-26 18:47:55 +0000

    g2: vs980 custom RIL for verizon

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -148,3 +148,15 @@ USE_SET_METADATA := false
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 
 TARGET_PROVIDES_LIBLIGHT := true
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+BOARD_RIL_CLASS := ../../../device/lge/g2-common/ril/
