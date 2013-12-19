0289a85ec8e2c3b2a35dd17fd5ee6e49f598411e
houst0nn <houstonn@gmail.com>
2013-12-19 09:26:13 +0100

    Revert "use audio caf"
    This reverts commit ab55ee21d6548afa2d38312f45d725551386dac7.

--- g2.mk
+++ g2.mk
@@ -34,7 +34,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/snd_soc_msm_Taiko:system/etc/snd_soc_msm/snd_soc_msm_Taiko \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf \
@@ -105,9 +106,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Audio Configuration
 PRODUCT_PROPERTY_OVERRIDES += \
-        persist.audio.handset.mic=digital \
-        persist.audio.fluence.mode=endfire \
-        af.resampler.quality=4
+	persist.audio.fluence.voicecall=true \
+	persist.audio.dualmic.config=endfire \
+	af.resampler.quality=4
 
 # Do not power down SIM card when modem is sent to Low Power Mode.
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -178,7 +179,7 @@ PRODUCT_PACKAGES += \
 	camera.g2
 
 PRODUCT_PACKAGES += \
-	audio_policy.msm8974 \
+	audio_policy.default \
 	audio.primary.msm8974 \
 	audio.a2dp.default \
 	audio.usb.default \
